48152652dba86754265c5ad85c0256aea2534345
justinkeung <justinkeung1018@gmail.com>
2022-10-06 22:29:39 +0100

    Implement Data.Char functions

--- Sequences.hs
+++ Sequences.hs
@@ -5,40 +5,40 @@ import Data.Char (ord, chr)
 -- Returns the first argument if it is larger than the second,
 -- the second argument otherwise
 maxOf2 :: Int -> Int -> Int
-maxOf2 
-  = undefined
+maxOf2 x y
+    | x > y     = x
+    | otherwise = y
 
 -- Returns the largest of three Ints
 maxOf3 :: Int -> Int -> Int -> Int
-maxOf3
-  = undefined
+maxOf3 x y z
+    | z > maxOf2 x y    = z
+    | otherwise         = maxOf2 x y
 
 -- Returns True if the character represents a digit '0'..'9';
 -- False otherwise
 isADigit :: Char -> Bool
-isADigit
-  = undefined
+isADigit char = '0' <= char && char <= '9'
 
 -- Returns True if the character represents an alphabetic
 -- character either in the range 'a'..'z' or in the range 'A'..'Z';
 -- False otherwise
 isAlpha :: Char -> Bool
-isAlpha
-  = undefined
+isAlpha char = 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z'
 
 -- Returns the integer [0..9] corresponding to the given character.
 -- Note: this is a simpler version of digitToInt in module Data.Char,
 -- which does not assume the precondition.
 digitToInt :: Char -> Int
 -- Pre: the character is one of '0'..'9'
-digitToInt
-  = undefined
+digitToInt digit = ord digit - ord '0'
 
 -- Returns the upper case character corresponding to the input.
 -- Uses guards by way of variety.
 toUpper :: Char -> Char
-toUpper 
-  = undefined
+toUpper char
+    | 'A' <= char && char <= 'Z'    = char
+    | otherwise                     = chr (ord char - ord 'a' + ord 'A')
 
 --
 -- Sequences and series
