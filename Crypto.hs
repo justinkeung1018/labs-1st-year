1e6f4fd06b57bda0b095f51e1eaf90c138eb7d62
justinkeung <justinkeung1018@gmail.com>
2022-10-11 11:12:45 +0100

    Fix formatting

--- Crypto.hs
+++ Crypto.hs
@@ -24,14 +24,13 @@ gcd m n
     | otherwise = gcd n (m `mod` n) 
 
 phi :: Int -> Int
-phi m
-  = length [num | num <- [1..m], gcd m num == 1]
+phi m = length [num | num <- [1..m], gcd m num == 1]
 
 -- Calculates (u, v, d) the gcd (d) and Bezout coefficients (u and v)
 -- such that au + bv = d
 computeCoeffs :: Int -> Int -> (Int, Int)
 computeCoeffs a b
-  | b == 0 = (1, 0)
+  | b == 0    = (1, 0)
   | otherwise = (v', u' - q * v')
   where
     (u', v') = computeCoeffs b r
@@ -39,8 +38,7 @@ computeCoeffs a b
 
 -- Inverse of a modulo m
 inverse :: Int -> Int -> Int
-inverse a m
-  = u `mod` m
+inverse a m = u `mod` m
   where
     (u, _) = computeCoeffs a m
 
@@ -59,8 +57,7 @@ smallestCoPrimeOf phi
 -- Generates keys pairs (public, private) = ((e, n), (d, n))
 -- given two "large" distinct primes, p and q
 genKeys :: Int -> Int -> ((Int, Int), (Int, Int))
-genKeys p q
-  = ((e, n), (d, n))
+genKeys p q = ((e, n), (d, n))
   where
     n = p * q
     e = smallestCoPrimeOf ((p - 1) * (q - 1))
@@ -68,12 +65,10 @@ genKeys p q
 
 -- RSA encryption/decryption
 rsaEncrypt :: Int -> (Int, Int) -> Int
-rsaEncrypt x (e, n)
-  = modPow x e n
+rsaEncrypt x (e, n) = modPow x e n
 
 rsaDecrypt :: Int -> (Int, Int) -> Int
-rsaDecrypt c (d, n)
-  = modPow c d n
+rsaDecrypt c (d, n) = modPow c d n
 
 -------------------------------------------------------------------------------
 -- PART 2 : symmetric encryption
@@ -81,23 +76,19 @@ rsaDecrypt c (d, n)
 
 -- Returns position of a letter in the alphabet
 toInt :: Char -> Int
-toInt char
-  = ord char - ord 'a'
+toInt char = ord char - ord 'a'
 
 -- Returns the n^th letter
 toChar :: Int -> Char
-toChar n
-  = chr (n + ord 'a')
+toChar n = chr (n + ord 'a')
 
 -- "adds" two letters
 add :: Char -> Char -> Char
-add char1 char2
-  = toChar ((toInt char1 + toInt char2) `mod` 26)
+add char1 char2 = toChar ((toInt char1 + toInt char2) `mod` 26)
 
 -- "substracts" two letters
 substract :: Char -> Char -> Char
-substract char1 char2
-  = toChar ((toInt char1 - toInt char2) `mod` 26)
+substract char1 char2 = toChar ((toInt char1 - toInt char2) `mod` 26)
 
 -- the next functions present
 -- 2 modes of operation for block ciphers : ECB and CBC
@@ -106,47 +97,37 @@ substract char1 char2
 -- ecb (electronic codebook) with block size of a letter
 --
 ecbEncrypt :: Char -> String -> String
-ecbEncrypt k m
-  = [add mi k | mi <- m]
+ecbEncrypt k m = [add mi k | mi <- m]
 
 ecbDecrypt :: Char -> String -> String
-ecbDecrypt k c
-  = [substract ci k | ci <- c]
+ecbDecrypt k c = [substract ci k | ci <- c]
 
 -- cbc (cipherblock chaining) encryption with block size of a letter
 -- initialisation vector iv is a letter
 -- last argument is message m as a string
 --
 cbcEncrypt :: Char -> Char -> String -> String
-cbcEncrypt k iv []
-  = []
-cbcEncrypt k iv x
-  = c1 : cbcEncrypt' k xs c1
+cbcEncrypt k iv [] = []
+cbcEncrypt k iv x  = c1 : cbcEncrypt' k xs c1
   where
     c1 = add (add x1 iv) k
     (x1: xs) = x
     cbcEncrypt' :: Char -> String -> Char -> String
-    cbcEncrypt' k [] prevChar
-        = []
-    cbcEncrypt' k x prevChar
-        = ci : cbcEncrypt' k xs ci
-        where
-            ci = add (add xi prevChar) k
-            (xi: xs) = x
+    cbcEncrypt' k [] prevChar = []
+    cbcEncrypt' k x  prevChar = ci : cbcEncrypt' k xs ci
+      where
+          ci = add (add xi prevChar) k
+          (xi: xs) = x
 
 cbcDecrypt :: Char -> Char -> String -> String
-cbcDecrypt k iv []
-  = []
-cbcDecrypt k iv c
-  = x1 : cbcDecrypt' k cs c1
+cbcDecrypt k iv [] = []
+cbcDecrypt k iv c  = x1 : cbcDecrypt' k cs c1
   where 
     x1 = substract (substract c1 k) iv
     (c1: cs) = c
     cbcDecrypt' :: Char -> String -> Char -> String 
-    cbcDecrypt' k [] prevChar 
-        = []
-    cbcDecrypt' k c prevChar
-        = xi : cbcDecrypt' k cs ci  
+    cbcDecrypt' k [] prevChar = []
+    cbcDecrypt' k c  prevChar = xi : cbcDecrypt' k cs ci  
         where 
             xi = substract (substract ci k) prevChar
             (ci: cs) = c
