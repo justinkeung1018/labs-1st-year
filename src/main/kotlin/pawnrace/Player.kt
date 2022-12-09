162cf4f09333ba7c36faeceb114c1625a5c733ed
justinkeung <justinkeung1018@gmail.com>
2022-12-09 00:37:21 +0000

    Add ab pruning

--- src/main/kotlin/pawnrace/Player.kt
+++ src/main/kotlin/pawnrace/Player.kt
@@ -6,7 +6,7 @@ class Player(val piece: Piece, var opponent: Player? = null) {
     private fun getAllValidMoves(game: Game): List<Move> = game.moves(piece)
 
     fun makeMove(game: Game): Move? {
-        fun minimax(game: Game, depth: Int, isMaximising: Boolean): Pair<Move?, Int> {
+        fun minimax(game: Game, depth: Int, alpha: Int, beta: Int, isMaximising: Boolean): Pair<Move?, Int> {
             fun staticEvaluate(board: Board): Int {
                 val pawns = getAllPawns(board);
                 // Distance from rank = 1, so white would want to maximise this distance
@@ -23,36 +23,46 @@ class Player(val piece: Piece, var opponent: Player? = null) {
             }
             val moves = if (isMaximising) game.moves(Piece.WHITE) else game.moves(Piece.BLACK)
             if (isMaximising) {
-                var maxScore = 0
+                var maxScore = Int.MIN_VALUE
                 var bestMove: Move? = null
+                var currAlpha = alpha
                 for (move in moves) {
                     game.applyMove(move)
-                    val pair = minimax(game, depth - 1, false)
+                    val pair = minimax(game, depth - 1, currAlpha, beta, false)
                     val score = pair.second + evaluateMoveType(move.type)
+                    currAlpha = currAlpha.coerceAtLeast(score)
                     if (score > maxScore) {
                         bestMove = move
                         maxScore = score
                     }
                     game.unapplyMove()
+                    if (beta <= currAlpha) {
+                        break
+                    }
                 }
                 return Pair(bestMove, maxScore);
             } else {
-                var minScore = 100;
+                var minScore = Int.MAX_VALUE
                 var bestMove: Move? = null
+                var currBeta = beta
                 for (move in moves) {
                     game.applyMove(move)
-                    val pair = minimax(game, depth - 1, true)
+                    val pair = minimax(game, depth - 1, alpha, currBeta,true)
                     val score = pair.second - evaluateMoveType(move.type)
+                    currBeta = currBeta.coerceAtMost(score)
                     if (score < minScore) {
                         bestMove = move
                         minScore = score
                     }
                     game.unapplyMove()
+                    if (currBeta <= alpha) {
+                        break
+                    }
                 }
                 return Pair(bestMove, minScore)
             }
         }
-        return minimax(game, 5, piece == Piece.WHITE).first
+        return minimax(game, 6, Integer.MIN_VALUE, Integer.MAX_VALUE, piece == Piece.WHITE).first
     }
 
 
