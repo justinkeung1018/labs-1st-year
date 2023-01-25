76f4396442fcc48bc62a27d33772dd8a63e5761e
justinkeung <justinkeung1018@gmail.com>
2023-01-25 09:43:19 +0000

    Rename variables

--- src/advancedstreams/CubeSupplier.java
+++ src/advancedstreams/CubeSupplier.java
@@ -7,10 +7,10 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class CubeSupplier implements Supplier<Integer> {
-  private int n;
+  private int number;
 
   public CubeSupplier() {
-    n = 1;
+    number = 1;
   }
 
   public static Stream<Integer> cubeStream() {
@@ -30,9 +30,9 @@ public class CubeSupplier implements Supplier<Integer> {
 
   @Override
   public Integer get() {
-    if (n > Math.pow(Integer.MAX_VALUE, 1D / 3)) {
+    if (number > Math.pow(Integer.MAX_VALUE, 1D / 3)) {
       throw new NoSuchElementException("Cube requested is too large.");
     }
-    return (int) Math.pow(n++, 3);
+    return (int) Math.pow(number++, 3);
   }
 }
--- src/rectangles/Point.java
+++ src/rectangles/Point.java
@@ -1,60 +1,60 @@
 package rectangles;
 
 public class Point {
-  private final int x;
-  private final int y;
+  private final int xCoordinate;
+  private final int yCoordinate;
 
   public Point(int x, int y) {
     if (x < 0 || y < 0) {
       throw new IllegalArgumentException("Coordinates must be non-negative.");
     }
-    this.x = x;
-    this.y = y;
+    this.xCoordinate = x;
+    this.yCoordinate = y;
   }
 
   public Point() {
-    this.x = 0;
-    this.y = 0;
+    this.xCoordinate = 0;
+    this.yCoordinate = 0;
   }
 
   public Point(int x) {
-    this.x = x;
-    this.y = 0;
+    this.xCoordinate = x;
+    this.yCoordinate = 0;
   }
 
   public int getX() {
-    return x;
+    return xCoordinate;
   }
 
   public int getY() {
-    return y;
+    return yCoordinate;
   }
 
   public Point setX(int newX) {
-    return new Point(newX, y);
+    return new Point(newX, yCoordinate);
   }
 
   public Point setY(int newY) {
-    return new Point(x, newY);
+    return new Point(xCoordinate, newY);
   }
 
   public boolean isLeftOf(Point other) {
-    return x < other.getX();
+    return xCoordinate < other.getX();
   }
 
   public boolean isRightOf(Point other) {
-    return x > other.getX();
+    return xCoordinate > other.getX();
   }
 
   public boolean isAbove(Point other) {
-    return y < other.getY();
+    return yCoordinate < other.getY();
   }
 
   public boolean isBelow(Point other) {
-    return y > other.getY();
+    return yCoordinate > other.getY();
   }
 
   public Point add(Point vector) {
-    return new Point(x + vector.getX(), y + vector.getY());
+    return new Point(xCoordinate + vector.getX(), yCoordinate + vector.getY());
   }
 }
